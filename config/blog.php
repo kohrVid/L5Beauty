17102462dcd9ba45d23905d36e0eddeac957f977
kohrVid <kohrVid@zoho.com>
2016-02-05 23:58:30 +0000

    Added Upload Manager

--- config/blog.php
+++ config/blog.php
@@ -2,5 +2,9 @@
 
 return [
 	"title" => "My Blog",
-	"posts_per_page" => 5
+	"posts_per_page" => 5,
+	"uploads" => [
+		"storage" => "s3",
+		"webpath" => "https://s3-eu-central-1.amazonaws.com/kohrvid-l5beauty",
+	],
 ];
